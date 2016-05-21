b31319a207b1924f19c77fb8561810b2782d18c8
Leonardo <leo.leo.gui70@gmail.com>
2016-05-21 12:22:57 -0300

    Adicionar Botão Cliente

--- PVFabrica/app/src/main/java/br/com/jsoftti/pvfabrica/activities/PrincipalActivity.java
+++ PVFabrica/app/src/main/java/br/com/jsoftti/pvfabrica/activities/PrincipalActivity.java
@@ -1,6 +1,7 @@
 package br.com.jsoftti.pvfabrica.activities;
 
 import android.content.Intent;
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -12,16 +13,22 @@ import br.com.jsoftti.pvfabrica.R;
 public class PrincipalActivity extends AppCompatActivity {
 
     ImageButton btnImgProdutos;
+    ImageButton btnImgClientes;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
         ImageButton btnImgProdutos = (ImageButton)findViewById(R.id.btnImgProdutos);
+        ImageButton btnImgClientes = (ImageButton)findViewById(R.id.btnImgClientes);
 
     }
 
     public void abrirActivityProduto(View view){
         Intent it = new Intent(this,ProdutosActivity.class);
+        startActivity(it);
+    }
+    public void abrirActivityCliente(View view){
+        Intent it = new Intent(this,ClientesActivity.class);
         startActivity(it);
     }
 }
